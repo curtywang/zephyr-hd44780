0b2f0e9025372fb3bda81efe0ef367d5103f76c4
Curtis Wang <ywang11@calstatela.edu>
2022-04-26 14:57:50 -0700

    fixed the prj.conf and wrote a helper

--- src/main.c
+++ src/main.c
@@ -1,79 +1,58 @@
 #include <zephyr.h>
 #include <sys/printk.h>
-#include <device.h>
-#include <drivers/gpio.h>
 #include <inttypes.h>
 // #include "button.h"
 // #include "led.h"
-#include "hd44780.h"
+#include <hd44780.h>
 // #include "adc.h"
 
-const uint8_t str1[] = "Hello from";
-const uint8_t str2[] = CONFIG_BOARD;
+const char str1[] = "Hello from";
+const char str2[] = CONFIG_BOARD;
+
+
+void write_to_lcd(char* input_str, size_t input_str_len) {
+    for(size_t i = 0; i < 16; i++) {
+        if (i >= input_str_len) {
+            hd44780_data(' ');
+        }
+        else {
+            hd44780_data(input_str[i]);
+        }
+    }
+}
+
 
 void main(void)
 {
-    uint32_t i;
-    uint8_t col = 0, btn = 0;
     printk("LCD Demo App starting!\n");
 
-    // button_init();
     hd44780_init();
-    //adc_init();
+    hd44780_cmd(HD44780_CMD_CLEAR, 0);
 
-    while (1)
+    while (true)
     {
-        hd44780_cmd(HD44780_CMD_CLEAR, 0);
+        hd44780_pos(0, 0);
+        write_to_lcd(str1, sizeof(str1) - 1);
 
-        hd44780_pos(0, col);
-        for(i = 0; i < sizeof(str1) - 1; i++)
-        {
-            hd44780_data(str1[i]);
-        }
-
-        hd44780_pos(1, col);
-        for(i = 0; i < sizeof(str2) - 1; i++)
-        {
-            hd44780_data(str2[i]);
-        }
-        col++;
-        if (col > 3)
-            col = 0;
-
-        hd44780_pos(0, 15);
-        // //btn = adc_sample();
-        // switch (btn)
-        // {
-        //     case btn_none:
-        //         hd44780_data(0xBC); // sort of smile, ROM Code: A00
-        //         break;
-
-        //     case btn_select:
-        //         hd44780_data(0xF4); // omega, ROM Code: A00
-        //         break;
-
-        //     case btn_left:
-        //         hd44780_data(0x7F); // left arrow, ROM Code: A00
-        //         break;
-
-        //     case btn_down:
-        //         hd44780_data('v');
-        //         break;
-
-        //     case btn_up:
-        //         hd44780_data('^');
-        //         break;
-
-        //     case btn_right:
-        //         hd44780_data(0x7E); // right arrow, ROM Code: A00
-        //         break;
-
-        //     default:
-        //         hd44780_data('E');
-        //         break;
-        // }
+        hd44780_pos(1, 0);
+        write_to_lcd(str2, sizeof(str2) - 1);
 
         k_sleep(K_MSEC(1000));
+        
+        hd44780_pos(0, 0);
+        for(size_t i = 0; i < 16; i++)
+        {
+            hd44780_data('O');
+        }
+
+        hd44780_pos(1, 0);
+        for(size_t i = 0; i < 16; i++)
+        {
+            hd44780_data('O');
+        }
+
+        k_sleep(K_MSEC(1000));
+        
     }
 }
 
